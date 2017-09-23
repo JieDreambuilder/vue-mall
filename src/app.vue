2c96f99db09ce5a481d97e92b9b8ed9849b96905
Jie <1275130954@qq.com>
2017-09-23 08:35:53 +0800

    update project

--- src/app.vue
+++ src/app.vue
@@ -1,97 +1,19 @@
 <template>
-    <div>
-        <mt-header id="hd" fixed title="传智播客.黑马程序员Vue.js"></mt-header>
-        <div id="content" class="routerview">
-
-            <transition name="fade" mode="out-in">
-                <router-view></router-view>
-            </transition>
-        </div>
-        <nav class="mui-bar mui-bar-tab">
-            <router-link class="mui-tab-item" to="/Home">
-                <span class="mui-icon mui-icon-home"></span>
-                <span class="mui-tab-label">首页</span>
-            </router-link>
-            <router-link class="mui-tab-item" to="/member">
-                <span class="mui-icon mui-icon-contact"></span>
-                <span class="mui-tab-label">会员</span>
-            </router-link>
-            <router-link class="mui-tab-item" to="/car">
-				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
-					<span v-show="goodscount>0" class="mui-badge" v-text="goodscount"></span>
-                    <span v-show="goodscount<=0" class="mui-badge" v-text="locdscount"></span>
-				</span>
-                <span class="mui-tab-label">购物车</span>
-            </router-link>
-            <router-link class="mui-tab-item" to="/search">
-                <span class="mui-icon mui-icon-search"></span>
-                <span class="mui-tab-label">搜索</span>
-            </router-link>
-        </nav>
-    </div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-    import shopcarkits from './components/goods/shopcarkits.js'
-
-    export default {
-        name: 'app',
-        data () {
-            return {
-                locdscount:0
-            }
-        },
-        methods:{
-
-        },
-        created (){
-//            let key  = 'shopcardata';
-//                //localStorage来存储购物车数据
-//            let locstorage = window.localStorage;
-//            let datas = locstorage.getItem(key);
-//            if(!datas){
-//                return;
-//            }
-//            let shopCardatas = JSON.parse(datas);
-//            let tmpcount = 0;
-//            shopCardatas.forEach((item)=>{
-//                tmpcount+=item.count;
-//        });
-//            this.locdscount = tmpcount;
-            this.locdscount = shopcarkits.reloadCount();
-        },
-        computed:{
-            goodscount: function(){
-//                console.log(this.$store.getters.shopCardatas);
-                return this.$store.getters.shopCarCount;
-            }
-        }
-    }
+export default {
+  name: 'app'
+}
 </script>
 
 <style>
-    @import '../statics/css/muihead.css';
-
-    body {
-        background-color: white;
-    }
-
-    .mint-tab-item-icon {
-        width: 48px;
-        height: 48px;
-        margin: 0 auto 5px
-    }
-
-    .routerview {
-        margin-top: 45px;
-    }
-
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity .2s
-    }
-
-    .fade-enter, .fade-leave-active {
-        opacity: 0
-    }
-
+  .left{
+    width:20%;
+    border: 1px solid gray;
+    float: left;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,84 +1,76 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import MintUI from 'mint-ui'
-import 'mint-ui/lib/style.css'
-import '../statics/css/mui.css'
-import '../statics/css/icons-extra.css'
-import '../statics/css/muihead.css'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import Vuex from 'vuex'
+import VueLazyload from 'vue-lazyload'
+import infiniteScroll from  'vue-infinite-scroll'
+import {currency} from './util/currency'
 
-import App from './App.vue'
+import './assets/css/base.css'
+import './assets/css/checkout.css'
+import './assets/css/login.css'
+import './assets/css/product.css'
 
-import VueRouter from 'vue-router'
-import vueResource from 'vue-resource'
-
-//import VueLazyload from 'vue-lazyload'
-
-Vue.use(VueRouter)
-Vue.use(vueResource)
-
-Vue.use(MintUI)  //注册全局组件
-import store from './vuex/store.js'
-
-
-//注册图片预览组件
-import VuePreview from 'vue-preview'
-Vue.use(VuePreview)
-
-//Vue.use(VueLazyload, {
-//preLoad: 1.3,
-//error: 'statics/images/loading.png',
-//loading: 'statics/images/loading.png',
-//attempt: 1
-//})
-
-import moment from '../statics/js/moment.js'
-//注册全局过滤器
-Vue.filter('dateFmt',(value,formatString)=>{
-	formatString = formatString || 'YYYY-MM-DD HH:mm:ss';
-	return moment(value).format(formatString); 
+Vue.use(infiniteScroll);
+Vue.use(Vuex);
+Vue.use(VueLazyload, {
+  loading: 'static/loading-svg/loading-bars.svg',
+  try: 3 // default 1
 })
 
-import Home from 'comp/Home/Home'
-import member from 'comp/member/member'
-import car from 'comp/car/car'
-import search from 'comp/search/search'
-//图文资讯组件
-import imgcontentnews from 'comp/news/ImgContentNew'
-//图文资讯详细内容显示组件
-import showNewsDetial from 'comp/news/showDetial'
-//图片分享组件
-import photolist from 'comp/photo/photolist'
-import photo from 'comp/photo/photo'
+Vue.filter("currency",currency);
+Vue.config.productionTip = false;
 
-//商品展示列表
-import goodlist from 'comp/goods/goodlist'
-import goodsInfo from 'comp/goods/goodsInfo'
-import goodscomment from 'comp/subcomp/comment'
-import goodsdesc from 'comp/goods/goodsdesc'
-
-const router = new VueRouter({
-	linkActiveClass:'mui-active', //可以修改router-link-active 为mui-active来解决tab栏的选中bug
-    mode: 'history',  //不想看到#号则配置这个 (可以用来解决与html本身锚点冲突的问题)
-    routes: [
-   		{ path: '/', redirect:'/Home'},
-        { path: '/Home', component: Home },
-        { path: '/member', component: member },
-        { path: '/car', component: car },
-        { path: '/search', component: search },
-        { path: '/news/imgcontentnews', component: imgcontentnews }, //图文资讯
-        { path: '/news/showdetial/:id', component: showNewsDetial }, //图文资讯详细
-        { path:'/photolist',component:photolist},  //图片分享列表
-        { path:'/photo/:id',component:photo},  //图片详情
-        { path:'/goodslist',component:goodlist},  //商品列表
-        { path:'/goods/info/:id',component:goodsInfo},  //商品详情
-        { name:'goodscomment', path:'/goods/comment/:id',component:goodscomment},  //商品评论
-        { name:'goodsdesc', path:'/goods/desc/:id',component:goodsdesc}  //商品评论
-    ]
-})
-
+const store = new Vuex.Store({
+  state: {
+    nickName:'',
+    cartCount:0
+  },
+  mutations: {
+    //更新用户信息
+    updateUserInfo(state, nickName) {
+      state.nickName = nickName;
+    },
+    updateCartCount(state,cartCount){
+      state.cartCount += cartCount;
+    }
+  }
+});
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  store:store,
-  router:router, //使用路由对象实例
-  render: h => h(App)
-})
-
+  store,
+  router,
+  mounted(){
+    this.checkLogin();
+    this.getCartCount();
+  },
+  methods:{
+    checkLogin(){
+      axios.get("users/checkLogin").then(res=> {
+        var res = res.data;
+        if (res.status == "0") {
+          this.$store.commit("updateUserInfo", res.result);
+        }else{
+          if(this.$route.path!="/goods"){
+            this.$router.push("/goods");
+          }
+        }
+      });
+    },
+    getCartCount(){
+      axios.get("users/getCartCount").then(res=>{
+        var res = res.data;
+        if(res.status=="0"){
+          this.$store.commit("updateCartCount",res.result);
+        }
+      });
+    }
+  },
+  template: '<App/>',
+  //render: h => h(App),
+  components: { App }
+});//.$mount('#app')
